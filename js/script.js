e902c1e6df88f3cd25a44eb35bd99ddb8dcea5d6
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-07-07 12:12:55 +0200

    modal + burger changes in select

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 let log = console.log;
+
+let burgerMenu = document.getElementById("burgerMenu");
+let select = document.getElementById("selectBurger");
+let burgerChosen = "modal-chosen";
+
+select.addEventListener("change", function () {
+  burgerMenu.classList.remove(burgerChosen);
+  if (select.value == "modal") {
+    burgerChosen = "modal-chosen";
+  }
+  burgerMenu.classList.add(burgerChosen);
+});
+
+// Modal Classic
+
+let modal = document.querySelector(".modal");
+let closeBtn = document.getElementById("closeBtn");
+
+burgerMenu.addEventListener("click", function () {
+  if (burgerChosen == "modal-chosen") {
+    modal.style.display = "flex";
+  }
+});
+
+closeBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
